d4a28208ca2d0a69671bd7615f7fe415f4107320
ankur <ankur_8wonder@yahoo.com>
2013-09-19 14:08:22 +0530

    Initialize repository

---
+++ second/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Second::Application.config.secret_token = '6128ab734eb78e0e40fcff1b2f07861cf0f273818aa9d883ed344c7771cfbb4a02dd013df63428fefe897a850a536ef8303260944400094284cfccacbfb3f858'
